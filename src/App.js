75cc1f6747f1a39637c4a215667802113b91b36d
dhibyoussef <youssefdhib28@gmail.com>
2025-11-07 21:16:12 +0100

    front completed sprint 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import AgentDashboard from "./pages/AgentDashboard";
