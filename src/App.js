ceacc75c0e5a1119c689afea733d9a989e0385ac
dhibyoussef <youssefdhib28@gmail.com>
2025-11-07 21:14:39 +0100

    front completed sprint 1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import AgentDashboard from './pages/AgentDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import ErrorBoundary from './components/home/layout_home/ErrorBoundary';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import AgentDashboard from "./pages/AgentDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import ErrorBoundary from "./components/home/layout_home/ErrorBoundary";
+import "./App.css";
 
 // Protected route component
 
-
 function App() {
-    return (
-        <ErrorBoundary>
-            <Router>
-                <div className="App">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route
-                            path="/agent"
-                            element={
-
-                                    <AgentDashboard />
-                            }
-                        />
-                        <Route
-                            path="/admin"
-                            element={
-
-                                    <AdminDashboard />
-
-                            }
-                        />
-                    </Routes>
-                </div>
-            </Router>
-        </ErrorBoundary>
-    );
+  return (
+    <ErrorBoundary>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/agent" element={<AgentDashboard />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
